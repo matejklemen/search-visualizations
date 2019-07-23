25c0039cd36f7ec93060f456f8d1698493a8a4c0
Matej Klemen <matej.klemen1337@gmail.com>
2019-07-24 00:06:47 +0200

    Prevent multiple steps of animation from happening simultaneously

--- src/searchVisualization.js
+++ src/searchVisualization.js
@@ -4,6 +4,8 @@ var algo = null;
 var SRC = "s";
 var DST = new Set(["k", "g"]);
 
+var transitionInProgress = false;
+
 var algoToFn = {
     "dfs": (() => depthFirstSearch(dwg, SRC, DST)),
     "bfs": (() => breadthFirstSearch(dwg, SRC, DST)),
@@ -13,30 +15,41 @@ var algoToFn = {
 
 function resetCurrentStep() {
     var nodeLabel = pathCache[0][step];
-    canvas.select("#" + nodesHardcoded[nodeLabel].idNode)
+    canvas.select("#" + nodeData[nodeLabel].idNode)
             .attr("stroke-width", "1px");
 }
 
 function nextStepTransition() {
-    var nextStep = (pathCache === null)? 0: d3.min([pathCache[0].length - 1, step + 1]);
-    // prevent endless repeating of last step of path
-    if(nextStep == step)
+    if(transitionInProgress)
         return;
+
+    transitionInProgress = true;
+    var nextStep = (pathCache === null)? 0: d3.min([pathCache[0].length - 1, step + 1]);
+    // last step of trace
+    if(nextStep == step) {
+        transitionInProgress = false;
+        return;
+    }
 
     if(step >= 0)
         resetCurrentStep();
 
     var nodeLabel = pathCache[0][nextStep];
-    canvas.select("#" + nodesHardcoded[nodeLabel].idNode)
+    canvas.select("#" + nodeData[nodeLabel].idNode)
             .transition()
             .duration(500)
             .attr("stroke-width", "10px")
             // prevent fast button clicks from messing up the visualization
-            .on("end", function() {step = nextStep});
+            .on("end", function() {step = nextStep; transitionInProgress = false;});
 }
 
 function prevStepTransition() {
+    if(transitionInProgress)
+        return;
+
+    transitionInProgress = true;
     var prevStep = d3.max([0 - 1, step - 1]);
+    // beginning of trace
     if(prevStep == step)
         return;
 
@@ -45,16 +58,18 @@ function prevStepTransition() {
 
     if(prevStep >= 0) {
         var nodeLabel = pathCache[0][prevStep];
-        canvas.select("#" + nodesHardcoded[nodeLabel].idNode)
+        canvas.select("#" + nodeData[nodeLabel].idNode)
                 .transition()
                 .duration(500)
                 .attr("stroke-width", "10px")
                 // prevent fast button clicks from messing up the visualization
-                .on("end", function() {step = prevStep});
+                .on("end", function() {step = prevStep; transitionInProgress = false;});
     }
-    else
+    else {
         // state before first step of trace
         step = prevStep;
+        transitionInProgress = false;
+    }
 }
 
 /* Recalculates path when the selected algorithm value changes. */
@@ -74,6 +89,6 @@ function clearVisualization() {
 }
 
 var dwg = new DirectedWeightedGraph();
-dwg.addNodesFrom(nodesHardcoded);
+dwg.addNodesFrom(nodeData);
 dwg.addEdgesFrom(edgeList);
 updateSelection();
